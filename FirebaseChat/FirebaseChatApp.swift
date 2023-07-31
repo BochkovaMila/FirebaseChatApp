81c7188ae28266367d030ea1b7ad60a49d26f924
Mila B <milabochkova03@gmail.com>
2023-07-31 15:59:33 +0300

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Mila B on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
