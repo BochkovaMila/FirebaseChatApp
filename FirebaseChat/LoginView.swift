f4134e8c96d153c1bfaabb02cc795a6d2c13ec28
Mila B <milabochkova03@gmail.com>
2023-07-31 16:32:48 +0300

    create a login page

---
+++ FirebaseChat/LoginView.swift
@@ -0,0 +1,82 @@
+//
+//  LoginView.swift
+//  FirebaseChat
+//
+//  Created by Mila B on 31.07.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+                    if !isLoginMode {
+                        Button {
+                            //avatar here
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                .ignoresSafeArea())
+        }
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Log into Firebase with existing credentials")
+        } else {
+            print("Register a new account inside of Firebase Auth and store image")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
